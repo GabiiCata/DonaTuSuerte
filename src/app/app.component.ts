7504d9df64fe11d9befa6e27a68b846c0ff933cc
GabiiCata <gabiicata94@gmail.com>
2020-05-02 06:57:23 -0300

    google sign in with firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import Swal from 'sweetalert2'
 import { RequestService } from './services/request.service';
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,19 +11,55 @@ export class AppComponent {
 
   status = 'No conectado'
   clase = 'alert-danger'
+  provider = undefined;
 
   constructor ( private request : RequestService ){
     
-    request.testRequest().then (data => {
+    this.testServerConnection()
+    this.provider = new firebase.auth.GoogleAuthProvider();
+
+  }
+
+   
+  
+  login (){
+    console.log ( this.provider );
+    
+    firebase.auth().signInWithPopup(this.provider).then(function(result:any) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+
+      console.log ( token )
+      console.log ( user )
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+  }
+
+    
+  testServerConnection (){
+
+    this.status = 'No conectado'
+    this.clase = 'alert-danger'
+
+    this.request.testRequest().then (data => {
       if (data ['message'] == 'json de prueba')
       {
-        this.status = 'Conectado'
+        this.status = 'Conectado al server'
         this.clase = 'alert-success'
       }
     })
-    
   }
-
    
 
   sweetRequest(){
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,25 @@ import { ProfileComponent } from './components/profile/profile.component';
 import { InfoComponent } from './components/info/info.component';
 import { HomeComponent } from './components/home/home.component'
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAYcl80CVK0LaQF1WOqoLRSOP1RDmBFhRs",
+  authDomain: "autoecoplant.firebaseapp.com",
+  databaseURL: "https://autoecoplant.firebaseio.com",
+  projectId: "autoecoplant",
+  storageBucket: "autoecoplant.appspot.com",
+  messagingSenderId: "146396366366",
+  appId: "1:146396366366:web:5d2717f327d3b1820d500f",
+  measurementId: "G-MMQR7FT4H6"
+};
+
+
+
+firebase.initializeApp(firebaseConfig);
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,7 +40,8 @@ import { HomeComponent } from './components/home/home.component'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    AngularFireModule
   ],
   providers: [],
   bootstrap: [AppComponent]
