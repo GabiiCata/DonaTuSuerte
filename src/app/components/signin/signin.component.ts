105a0cea98aa6628149f016a264a6d9165b78132
GabiiCata <gabiicata94@gmail.com>
2020-05-06 23:09:23 -0300

    creacion component sorteo

---
+++ src/app/components/signin/signin.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit } from '@angular/core';
+import { RequestService } from '../../services/request.service';
+import * as firebase from 'firebase';
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.css']
+})
+export class SigninComponent implements OnInit {
+
+  provider = undefined;
+
+  constructor( private request : RequestService ) { 
+    this.provider = new firebase.auth.GoogleAuthProvider();
+  }
+
+  user = {
+    "email": "",
+    "password": "",
+    "photo": "",
+    "firstName": "",
+    "lastName": "",
+    "country": "",
+    "dateOfBirth": "",
+    "address": {
+      "street": "",
+      "city": "",
+      "state": "",
+      "zip": "",
+    },
+    "phone": "",
+    "conditions": "",
+    "score": "",
+  }
+
+  signIn(){
+    console.log ( this.user )
+
+    this.request.signIn( this.user );
+  }
+
+
+
+  login (){
+    console.log ( this.provider );
+
+    firebase.auth().signInWithPopup(this.provider).then(function(result:any) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+      console.log ( user  )
+
+      console.log ( token )
+      
+
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+
+      console.log ( errorCode + errorMessage )
+    });
+  }
+
+  
+
+  ngOnInit() {
+  }
+
+}
--- src/app/services/request.service.ts
+++ src/app/services/request.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +8,26 @@ export class RequestService {
 
   constructor(private http: HttpClient) { }
 
-  url = 'https://hackaton-leaf.herokuapp.com/api/v1/test';
+  url = 'https://hackaton-leaf.herokuapp.com/api/v1';
 
   testRequest(){
     return this.http.get( this.url ).toPromise().then( data => { return data });
+  }
+
+
+  signIn( body )
+  {
+    let uri = "/users/register";
+
+    // let httpHeaders = new HttpHeaders({
+    //   'Authorization' : 'Bearer ' + localStorage.getItem('token')
+    // });
+
+    this.http.post ( this.url + uri , body ).toPromise()
+      .then( data => {
+        console.log ( data ) ;
+      }) 
+
   }
 
 }
